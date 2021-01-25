a83b704ca2f7cd30d80745c5da2fc1b6e9d41198
seymakara888 <seymakara888@gmail.com>
2021-01-25 19:58:19 +0300

    v0.2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express    = require("express");
 const bodyParser = require("body-parser");
+const https      = require("https");
 const app        = express();
 const mongoose   = require("mongoose");
 app.set("view engine", "ejs");
@@ -16,13 +17,87 @@ const guzelSozSema = {
   icerik : String
 };
 const GuzelSoz = mongoose.model("GuzelSoz", guzelSozSema);
+/*
+var guzelSoz1 = new GuzelSoz({
+  kategori : "Kurtlar Vadisi",
+  icerik : "Sonunu düşünen kahraman olamaz."
+});
+var guzelSoz2 = new GuzelSoz({
+  kategori : "Kurtlar Vadisi",
+  icerik : "Ölüm Ölüm dediğin nedir ki gülüm? ben senin için yaşamayı göze almışım.."
+});
+var guzelSoz3 = new GuzelSoz({
+  kategori : "Kurtlar Vadisi",
+  icerik : "Hukuk insanı sadece yaşatmaz, öldürür de."
+});
+guzelSoz1.save();
+guzelSoz2.save();
+guzelSoz3.save();
+*/
+/*
+//  TÜM GÜZEL SÖZLERİ JSON FORMATINDA RETURN EDELİM..
+app.get("/api/guzelsozler", function(req, res){
+  GuzelSoz.find({} , function(err, gelenVeriler){
+    res.send(gelenVeriler);
+  })
+});
+//  ID'sine göre JSON FORMATINDA 1 TANE GÜZEL SÖZ RETURN EDELİM.
+app.get("/api/guzelsozler/:id", function(req, res){
+  var gelenId = req.params.id;
+  GuzelSoz.findOne({_id : gelenId} , function(err, gelenVeri){
+    res.send(gelenVeri);
+  });
+});
 
+app.post("/api/guzelsozkayit", function(req, res){
+  var kategori = req.body.kategori;
+  var icerik   = req.body.icerik;
+  var guzelSoz = new GuzelSoz({
+    kategori : kategori,
+    icerik : icerik
+  });
+  guzelSoz.save(function(err){
+    if(!err)
+      res.send("Başarıyla kayıt edildi.");
+    else
+      res.send(err);
+  });
+});
+
+app.delete("/api/guzelsozsil/:id", function(req, res){
+    GuzelSoz.deleteOne({_id : req.params.id } , function(err){
+      if(!err)
+        res.send("Başarıyla silindi.")
+      else
+        res.send(err);
+    });
+});
+
+app.put("/api/guzelsozdegistirme/:id", function(req, res){
+    var kategoriGelen = req.body.kategori;
+    var guzelsozGelen = req.body.icerik;
+    GuzelSoz.update( {_id : req.params.id}, {kategori : kategoriGelen, icerik : guzelsozGelen}, {overwrite: true}, function(err){
+        if(!err)
+          res.send("Kayıt başarıyla güncellendi.");
+        else
+          res.send(err);
+    });
+});
+
+app.patch("/api/guzelsozguncelleme/:id", function(req, res){
+    GuzelSoz.update({ _id : req.params.id } , { $set : req.body } , function(err){
+        if(!err)
+          res.send("Kayıt başarıyla güncellendi.");
+        else
+          res.send(err);
+    });
+});
+*/
 app.get("/", function(req, res){
   GuzelSoz.find({}, function(err, gelenSozler){
     res.render("anasayfa", {sozler : gelenSozler});
   });
 });
-
 app.route("/api/guzelsoz/:id")
     .get(function(req, res){
       GuzelSoz.findOne({_id : req.params.id} , function(err, gelenVeri){
@@ -34,7 +109,7 @@ app.route("/api/guzelsoz/:id")
       var icerikGelen   = req.body.icerik;
       GuzelSoz.update({_id : req.params.id} , {kategori : kategoriGelen, icerik : icerikGelen}, {overwrite: true}, function(err){
         if(!err)
-          res.send("Kayıt başarıyla güncellendi.");
+          res.send({sonuc : "Kayıt başarıyla güncellendi."});
         else
           res.send(err);
       });
@@ -42,18 +117,23 @@ app.route("/api/guzelsoz/:id")
     .patch(function(req, res){
       GuzelSoz.update({_id : req.params.id} , {$set : req.body}, function(err){
         if(!err)
-          res.send("Kayıt başarıyla güncellendi.");
+          res.send({sonuc : "Kayıt başarıyla güncellendi."});
         else
           res.send(err);
       })
     })
     .delete(function(req, res){
-      GuzelSoz.deleteOne({_id : req.params.id}, function(err){
-        if(!err)
-          res.send("Kayıt başarıyla silindi.");
-        else
-          res.send(err);
-      })
+      var sifre = req.body.sifre;
+      if(sifre == "parola1234"){
+        GuzelSoz.deleteOne({_id : req.params.id}, function(err){
+          if(!err)
+            res.send({sonuc : "Kayıt başarıyla silindi."});
+          else
+            res.send(err);
+        })
+      }else{
+        res.send({sonuc : "Şifre hatalı."});
+      }
     });
 app.route("/api/guzelsozler")
     .get(function(req, res){
@@ -71,21 +151,64 @@ app.route("/api/guzelsozler")
        });
        guzelSoz.save(function(err){
           if(!err)
-            res.send("Kayıt başarıyla oluşturuldu.");
+            res.send( {sonuc : "Kayıt başarıyla oluşturuldu."} );
           else
             res.send(err);
        });
     })
     .delete(function(req, res){
-      GuzelSoz.deleteMany({}, function(err){
-        if(!err)
-          res.send("Tüm kayıtlar başarıyla silindi.");
-        else
-          res.send(err);
-      });
+      var sifre = req.body.sifre;
+      if(sifre == "parola1234"){
+        GuzelSoz.deleteMany({}, function(err){
+          if(!err)
+            res.send( {sonuc : "Tüm kayıtlar başarıyla silindi."} );
+          else
+            res.send(err);
+        });
+      }else{
+        res.send({sonuc : "Şifre hatalı."});
+      }
     });
 
 
+    app.get("/admin", function(req, res){
+        // 1. Alternatif
+        /*GuzelSoz.find({}, function(err, gelenGuzelSozler){
+          res.render("admin", {guzelsozler : gelenGuzelSozler});
+        })*/
+        var link = "https://seyma-guzelsozler.herokuapp.com/api/guzelsozler";
+        https.get(link , function(response){
+          response.on("data", function(gelenGuzelSozler){
+            // gelenGuzelSozler -> byte türünde gelmişti.
+            var guzelSozler = JSON.parse(gelenGuzelSozler);
+            res.render("admin", {sozler : guzelSozler});
+          })
+        });
+    });
+
+    //https://guzelsozler.herokuapp.com/api/guzelsoz/600c683c986f50001534a062
+    app.post("/kayit-sil", function(req, res){
+        var id = req.body._id;
+        var link = "https://seyma-guzelsozler.herokuapp.com/api/guzelsoz/"+id;
+        const gonderilecekler = JSON.stringify({
+          sifre: 'parola1234'
+        })
+        const secenekler = {
+          method: 'DELETE',
+          headers: {
+            'Content-type': 'application/json',
+            'Content-Length': gonderilecekler.length
+          }
+        }
+        const baglanti = https.request(link, secenekler, function(response) {
+          response.on('data', function(gelenVeri) {
+            var sonuc = JSON.parse(gelenVeri);
+            res.send(sonuc);
+          })
+        })
+        baglanti.write(gonderilecekler);
+        baglanti.end();
+    });
 
     let port = process.env.PORT;
     if(port == "" || port == null){
